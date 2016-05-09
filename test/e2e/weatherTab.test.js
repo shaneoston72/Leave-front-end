f2822609b0e697c04828f06860b8c7ff21947320
Misa Ogura <misa.ogura01@gmail.com>
2016-05-09 09:51:08 +0100

    Tests running for weather API

--- test/e2e/weatherTab.test.js
+++ test/e2e/weatherTab.test.js
@@ -4,11 +4,14 @@ describe('WeatherTab', function(){
       browser.get('/#/tab/weather');
     });
 
-    fit('should display current weather', function() {
+    it('should display current weather', function() {
       var weatherTitle = element(by.id('weatherTitle'));
       var currentWeather = element(by.id('currentWeather'));
+      var weatherDesc = element(by.id('weatherDesc'));
+      var weatherTemp = element(by.id('weatherTemp'));
       expect(weatherTitle.isPresent()).toBe(true);
       expect(currentWeather.isPresent()).toBe(true);
-      expect(currentWeather.textContent()).toBeDefined();
+      expect(weatherDesc.isPresent()).toBe(true);
+      expect(weatherTemp.isPresent()).toBe(true);
     });
   });
