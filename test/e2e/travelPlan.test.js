c3fbe065211e77a31169587ff50ae9f1a3eeb2e7
Misa Ogura <misa.ogura01@gmail.com>
2016-05-11 10:31:01 +0100

    Test not working for stationlist, need to come back later

--- test/e2e/travelPlan.test.js
+++ test/e2e/travelPlan.test.js
@@ -24,4 +24,34 @@ describe('TravelPlan', function(){
       expect(fromList.isPresent()).toBe(true);
     });
 
+    // not working test - unable to click station list
+    xit('should have a notification for an alarm', function() {
+      travelLink.click();
+
+      fromStation = element(by.id('fromStation'));
+      fromStation.click();
+      var fromSt = element(by.cssContainingText('h2','Turnpike Lane'));
+      fromSt.click();
+
+      toStation = element(by.id('toStation'));
+      toStation.click();
+      var toSt = element(by.xpath('.//*[.="Aldgate East"]'));
+      toSt.click();
+
+      arrivalTime = element(by.id('arrivalTime'));
+      arrivalTime.click();
+
+      var hour = element(by.model('bind.hour'));
+      var minute = element(by.model('bind.minute'));
+      hour.sendKeys('12');
+      minute.sendKeys('00');
+      okButton = element(by.buttonText('OK'));
+      okButton.click();
+
+      getTripButton = element(by.id('getTrip'));
+      getTripButton.click();
+
+      alarmSetText = element(by.id('alarmSetText'));
+      expect($('alarmSetText').isDisplayed()).toBeTruthy();
+    });
   });
